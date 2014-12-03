d6bf3d631a676832d82269b53ab7bad94d4e221a
me <noemail@nowhere.com>
2014-12-03 00:26:49 -0700

    More fiddling to make things a bit better.  Still need to add some QwtLabels with max/min/average power levels, but otherwise it is nearing completion

--- sources/SweepInspector.cpp
+++ sources/SweepInspector.cpp
@@ -9,12 +9,34 @@ $Id: doer-demod-display.cpp 815 2014-10-27 19:20:22Z npotts $
 */
 #include "SweepInspector.h"
 
+FreqScaleDraw::FreqScaleDraw() : QwtScaleDraw() {}
+FreqScaleDraw::~FreqScaleDraw() {}
+QwtText FreqScaleDraw::label(double v) const {
+  //convert 1320 to 1.3KHz, 15012402 to 15.0MHz ext
+  QString rtn;
+  switch ( (int) std::log10(v)) {
+    case 9: rtn = QString("%1 GHz").arg(v/1e9, 0, 'f', 2); break;
+    case 8:
+    case 7:
+    case 6: rtn = QString("%1 MHz").arg(v/1e6, 0, 'f', 2); break;
+    case 5:
+    case 4:
+    case 3: rtn = QString("%1 kHz").arg(v/1e3, 0, 'f', 2); break;
+    case 2:
+    case 1:
+    case 0: rtn = QString("%1 Hz").arg(v, 0, 'f', 2); break;
+  }
+  return QwtText(rtn);
+}
+
 SweepInspector::~SweepInspector() {
   /** destroy stuffs  */
   delete(d_curve);
   delete(canvas);
   delete(zoomer);
   delete(panner);
+  delete(picker);
+  delete(grid);
 }
 SweepInspector::SweepInspector(QWidget *parent) : QWidget(parent), data(NULL), d_curve(NULL) {
   setupUi(this);
@@ -29,14 +51,35 @@ SweepInspector::SweepInspector(QWidget *parent) : QWidget(parent), data(NULL), d
   canvas->setBorderRadius(0);
   plot->setCanvas(canvas);
 
+  //Allow zooming / panning
   zoomer = new QwtPlotZoomer( canvas );
   zoomer->setRubberBandPen( QColor( Qt::white ) );
   zoomer->setTrackerPen( QColor( Qt::white ) );
-  zoomer->setMousePattern( QwtEventPattern::MouseSelect2, Qt::RightButton, Qt::ControlModifier );
-  zoomer->setMousePattern( QwtEventPattern::MouseSelect3, Qt::RightButton );
-
   panner = new QwtPlotPanner( canvas );
   panner->setMouseButton( Qt::MidButton );
+
+  //Show the X/Y markers that follow the mouse
+  picker = new QwtPlotPicker( QwtPlot::xBottom, QwtPlot::yLeft, QwtPlotPicker::CrossRubberBand, QwtPicker::AlwaysOn, canvas);
+  picker->setStateMachine( new QwtPickerTrackerMachine() );
+  picker->setRubberBandPen( QColor( Qt::cyan ) );
+  picker->setRubberBand( QwtPicker::CrossRubberBand );
+  picker->setTrackerPen( QColor( Qt::cyan ) );
+
+  //Setup grid
+  grid = new QwtPlotGrid();
+  grid->enableXMin( true );
+  grid->enableYMin( true );
+  QColor color(Qt::gray); color.setAlpha(128);
+  grid->setMajorPen( color, 1, Qt::DotLine );
+  grid->setMinorPen( color, 1, Qt::DotLine );
+  grid->attach( plot );
+
+  //format in kHz, MHz, GHz, not raw values
+  plot->setAxisScaleDraw(QwtPlot::xBottom, new FreqScaleDraw);
+
+  //tmax.setFont( QFont( "Helvetica", 10, QFont::Bold ) ); tmax.setColor( Qt::green );
+  //tmin.setFont( QFont( "Helvetica", 10, QFont::Bold ) ); tmin.setColor( Qt::green );
+  //tavg.setFont( QFont( "Helvetica", 10, QFont::Bold ) ); tavg.setColor( Qt::green );
 
   //connects
   connect(timeIndex, SIGNAL(valueChanged(int)), this, SLOT(loadSweep(int)));
@@ -49,6 +92,7 @@ void SweepInspector::setpData(QHoundData *newd) {
   timeIndex->setValue(0);
 }
 void SweepInspector::sliderMoved(int index) {
+  //update text despite not letting go
   if (data == NULL) 
     timestamp->setText("");
   else
@@ -58,16 +102,10 @@ void SweepInspector::loadSweep(int index) {
   //load in new sweep values from data
   if (data == NULL) return;
   sliderMoved(index);
-  if (d_curve) {
-    d_curve->attach(NULL);
-    delete(d_curve);
-  }
-  plot->detachItems(QwtPlotItem::Rtti_PlotItem, true);
-  plot->detachItems(QwtPlotItem::Rtti_PlotCurve);
+
+  //remove old data and get new
+  if (d_curve) d_curve->attach(NULL);
   plot->detachItems();
-  plot->replot();
-  plot->repaint();
-  
   fsweep sweep = data->getSweep(index);
 
   d_curve = new QwtPlotCurve( timestamp->text() ); //Qwt will delete() this when its done with it
@@ -80,9 +118,24 @@ void SweepInspector::loadSweep(int index) {
   range freqs = data->limits(FREQ);
   plot->setAxisScale(QwtPlot::xBottom, freqs.first, freqs.second, (freqs.second - freqs.first)/ 5.0);
   plot->setAxisScale(QwtPlot::yLeft, -135, 20, 10.0);
-  plot->replot();
-  plot->repaint();
-
+  plot->setTitle( QString("RF Sweep @ %1").arg(timestamp->text()) );
   //set maximum zoom out
   zoomer->setZoomBase(QRectF(QPointF(freqs.first, -60), QPointF(freqs.second, -135)));
+  zoomer->zoomBase();
+
+
+  //find max, min, and average values and drop it on plot as well
+  double max = freqs.first, min = freqs.first, average=0;
+  for(int i=0; i < sweep.size(); i++) {
+    max = std::max(max, sweep.at(i).y());
+    min = std::min(min, sweep.at(i).y());
+    average += sweep.at(i).y();
+  } average /= sweep.size();
+
+  // tmax.setText(QString("Max: %1 dBm").arg(max));
+  // tmin.setText(QString("Min: %1 dBm").arg(min));
+  // tavg.setText(QString("Avg: %1 dBm").arg(average));
+
+  plot->replot();
+  plot->repaint();
 }
--- sources/SweepInspector.h
+++ sources/SweepInspector.h
@@ -9,21 +9,34 @@ $Id: doer-demod-display.h 725 2014-05-22 21:28:57Z npotts $
 */
 
 #pragma once
+#ifndef QT_STATIC_CONST
 #define QT_STATIC_CONST static const
+#endif
+#include <cmath>
 #include "ui_SweepInspector.h"
 #include "SweepInspector.moc"
 #include <QtWidgets>
 #include <QtCore>
-#include <qwt_plot_curve.h>
-#include <qwt_symbol.h>
-#include <qwt_plot_zoomer.h>
-#include <qwt_plot_panner.h>
-#include <qwt_plot_marker.h>
-#include <qwt_plot_grid.h>
-#include <qwt_plot_curve.h>
+#include <qwt_picker_machine.h>
 #include <qwt_plot_canvas.h>
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_plot_marker.h>
+#include <qwt_plot_panner.h>
+#include <qwt_plot_textlabel.h>
+#include <qwt_plot_zoomer.h>
+#include <qwt_scale_draw.h>
+#include <qwt_symbol.h>
 
 #include "SHData.h"
+
+//formats 403239422 as 403.2MHz
+class FreqScaleDraw : public QwtScaleDraw {
+public:
+    FreqScaleDraw();
+    virtual ~FreqScaleDraw();
+    virtual QwtText label(double) const;
+};
 
 class SweepInspector:public QWidget, public Ui::sweepinspector {
 Q_OBJECT
@@ -38,7 +51,9 @@ private:
   QHoundData * data;
   QwtPlotCurve *d_curve;
   QwtPlotCanvas *canvas;
-  QwtPlotZoomer* zoomer;
+  QwtPlotZoomer *zoomer;
   QwtPlotPanner *panner;
-  
+  QwtPlotPicker *picker;
+  QwtPlotGrid *grid;
+  //QwtTextLabel tmax, tmin, tavg;
 };
